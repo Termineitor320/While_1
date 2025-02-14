0c8fc3fafe2f15769d2c0b42926be2aee5e07a78
Termineitor320 <danieldiaz@colegioguanenta.edu.co>
2025-02-14 06:41:28 -0500

    se agrego el codigo python

---
+++ while_1.py
@@ -0,0 +1,14 @@
+print("--------------------------------")
+#input 
+n = int(input("digite el valor ded n: "))
+print("--------------------------------")
+#processing
+s = 0
+i = 1 
+while i <= n:
+    s= s + i
+    i= i + 1
+print("la suma de los " + str(n) + " primeros numeros naturales es " + str(s) )
+
+ 
+    
